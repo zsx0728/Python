2d15793af4277c82b92200af5d31e142fc2cddb3
zsx0728 <35521209+zsx0728@users.noreply.github.com>
2022-01-12 17:14:33 +0800

    Update zabbix_ip.py

--- api/zabbix_ip.py
+++ api/zabbix_ip.py
@@ -1,3 +1,5 @@
+#本脚本需与zabbix_base.py脚本放在统一目录下
+#本脚本提取zabbix内所有设备IP，然后写入文件
 import zabbix_base
 token = zabbix_base.get_token()
 data = {
@@ -18,7 +20,6 @@ result = zabbix_base.zabbix_api_common(data)
 print(len(result["result"]))
 with open('zabbix_ip.txt','a+') as ip_out:
   for ip_number in range(len(result["result"])):
-    #print(result["result"][ip_number]["interfaces"][0]["ip"])
     ip_out.write(result["result"][ip_number]['interfaces'][0]['ip'] + ' ' + result["result"][ip_number]['host'] + '\n')
     ip_out.flush()
   ip_out.close()
